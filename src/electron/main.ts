cc1254ccba5dd984f939d13a821c7c8fa70c8817
yangzhie <arnavmalasi2212@gmail.com>
2025-09-15 13:52:47 +1000

    Display most recent clipboards in Electron console.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,12 +1,28 @@
-import {app, BrowserWindow} from "electron";
+import {app, BrowserWindow, clipboard} from "electron";
 import path from "path";
 import { devMode } from "./utils.js";
 
 app.on("ready", () => {
-    const mainWindow = new BrowserWindow({});
+        const mainWindow = new BrowserWindow({});
 
-    if (devMode()) {
-        mainWindow.loadURL('http://localhost:9999/');
-    } else {
-        mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
-    }});
+        if (devMode()) {
+            mainWindow.loadURL('http://localhost:9999/');
+        } else {
+            mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+        }
+
+        monitorCopy();
+    }  
+);
+
+function monitorCopy() {
+    let lastText = clipboard.readText();
+    
+    setInterval(() => {
+        const currentText = clipboard.readText();
+        if(currentText !== lastText) {
+            console.log("Copied: ", currentText);
+            lastText = currentText;
+        }
+    }, 1000);
+}
---
+++ src/electron/preload.cts
@@ -0,0 +1,5 @@
+const electron = require("electron");
+
+electron.contextBridge.exposeInMainWorld("electron", {
+    
+})
